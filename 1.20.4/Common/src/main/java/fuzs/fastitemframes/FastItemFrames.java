006f00dde5ed013a8be5b11c940554b88998189b
Fuzss <28218241+Fuzss@users.noreply.github.com>
2024-06-04 22:41:10 +0200

    cauldron interaction + render item name

--- 1.20.4/Common/src/main/java/fuzs/fastitemframes/FastItemFrames.java
+++ 1.20.4/Common/src/main/java/fuzs/fastitemframes/FastItemFrames.java
@@ -6,7 +6,9 @@ import fuzs.puzzleslib.api.core.v1.ModConstructor;
 import fuzs.puzzleslib.api.event.v1.entity.ServerEntityLevelEvents;
 import fuzs.puzzleslib.api.event.v1.entity.player.PlayerInteractEvents;
 import fuzs.puzzleslib.api.event.v1.level.BlockEvents;
+import net.minecraft.core.cauldron.CauldronInteraction;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.item.Items;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -26,6 +28,12 @@ public class FastItemFrames implements ModConstructor {
         ServerEntityLevelEvents.LOAD.register(ItemFrameHandler::onEntityLoad);
         PlayerInteractEvents.USE_ENTITY.register(ItemFrameHandler::onUseEntity);
         PlayerInteractEvents.ATTACK_ENTITY.register(ItemFrameHandler::onAttackEntity);
+    }
+
+    @Override
+    public void onCommonSetup() {
+        CauldronInteraction.WATER.map().put(Items.ITEM_FRAME, ItemFrameHandler::itemFrameCauldronInteraction);
+        CauldronInteraction.WATER.map().put(Items.GLOW_ITEM_FRAME, ItemFrameHandler::itemFrameCauldronInteraction);
     }
 
     public static ResourceLocation id(String path) {
--- 1.20.4/Common/src/main/java/fuzs/fastitemframes/client/renderer/blockentity/ItemFrameBlockRenderer.java
+++ 1.20.4/Common/src/main/java/fuzs/fastitemframes/client/renderer/blockentity/ItemFrameBlockRenderer.java
@@ -4,6 +4,7 @@ import com.google.common.collect.ImmutableMap;
 import com.mojang.blaze3d.vertex.PoseStack;
 import fuzs.fastitemframes.FastItemFrames;
 import fuzs.fastitemframes.world.level.block.entity.ItemFrameBlockEntity;
+import net.minecraft.client.Minecraft;
 import net.minecraft.client.renderer.MultiBufferSource;
 import net.minecraft.client.renderer.blockentity.BlockEntityRenderer;
 import net.minecraft.client.renderer.blockentity.BlockEntityRendererProvider;
@@ -13,6 +14,8 @@ import net.minecraft.client.resources.model.ModelResourceLocation;
 import net.minecraft.core.Direction;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.entity.decoration.ItemFrame;
+import net.minecraft.world.phys.BlockHitResult;
+import net.minecraft.world.phys.HitResult;
 import net.minecraft.world.phys.Vec3;
 
 import java.util.Map;
@@ -48,6 +51,7 @@ public class ItemFrameBlockRenderer implements BlockEntityRenderer<ItemFrameBloc
                         direction.getStepY() * -0.46875F,
                         direction.getStepZ() * -0.1675F
                 );
+
                 // the internal item frame entity is always set to invisible so the block itself does not render as it is handled as a block model
                 // we only use the renderer for the contained item
                 if (!blockEntity.isInvisible()) {
@@ -56,6 +60,11 @@ public class ItemFrameBlockRenderer implements BlockEntityRenderer<ItemFrameBloc
                             direction.getStepZ() * 0.0625F
                     );
                 }
+
+                if (this.shouldShowName(blockEntity, itemFrame)) {
+                    renderer.renderNameTag(itemFrame, itemFrame.getDisplayName(), poseStack, buffer, packedLight);
+                }
+
                 renderer.render(itemFrame, 0.0F, partialTick, poseStack, buffer, packedLight);
                 poseStack.popPose();
             }
@@ -70,5 +79,19 @@ public class ItemFrameBlockRenderer implements BlockEntityRenderer<ItemFrameBloc
         } else {
             return BlockEntityRenderer.super.shouldRender(blockEntity, cameraPos);
         }
+    }
+
+    protected boolean shouldShowName(ItemFrameBlockEntity blockEntity, ItemFrame entity) {
+        if (Minecraft.renderNames() && !entity.getItem().isEmpty() && entity.getItem().hasCustomHoverName()) {
+            Minecraft minecraft = Minecraft.getInstance();
+            HitResult hitResult = minecraft.hitResult;
+            if (hitResult != null && hitResult.getType() == HitResult.Type.BLOCK && blockEntity.getBlockPos().equals((((BlockHitResult) hitResult).getBlockPos()))) {
+                double distanceToEntity = minecraft.gameRenderer.getMainCamera().getPosition().distanceToSqr(entity.position());
+                double permittedDistance = entity.isDiscrete() ? 32.0 : 64.0;
+                return distanceToEntity < (permittedDistance * permittedDistance);
+            }
+        }
+
+        return false;
     }
 }
